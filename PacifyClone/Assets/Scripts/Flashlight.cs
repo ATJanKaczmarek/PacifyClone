ab0173388c2a0ef81311ebd3f90385a841154844
ATJanKaczmarek <53752609+ATJanKaczmarek@users.noreply.github.com>
2020-06-10 23:07:14 +0200

    Added Flashlight

---
+++ PacifyClone/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject lightSource;
+    private bool flashlightOn;
+
+    private void Start()
+    {
+        lightSource.SetActive(false);
+        flashlightOn = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F) && !flashlightOn)
+        {
+            lightSource.SetActive(true);
+            flashlightOn = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.F) && flashlightOn)
+        {
+            lightSource.SetActive(false);
+            flashlightOn = false;
+        }
+    }
+}
