b6a57175d4c54a5897049d9dd194e2ac45f5fda9
ATJanKaczmarek <53752609+ATJanKaczmarek@users.noreply.github.com>
2020-06-11 17:27:27 +0200

    Revert "Flashlight, PostProcessing, Scripts, Animation"
    This reverts commit 3989feb8a50cdcc5e1b87743465da9dde9b51101.

--- PacifyClone/Assets/Scripts/Flashlight.cs
+++ PacifyClone/Assets/Scripts/Flashlight.cs
@@ -7,12 +7,8 @@ public class Flashlight : MonoBehaviour
     public GameObject lightSource;
     private bool flashlightOn;
 
-    public AudioSource flashlightTrigger;
-
-    private Animator animator;
     private void Start()
     {
-        animator = GetComponent<Animator>();
         lightSource.SetActive(false);
         flashlightOn = false;
     }
@@ -21,31 +17,13 @@ public class Flashlight : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.F) && !flashlightOn)
         {
-            animator.SetBool("FlashLightOn", true);  
+            lightSource.SetActive(true);
             flashlightOn = true;
-            StartCoroutine(lightDelayed());
         }
         else if(Input.GetKeyDown(KeyCode.F) && flashlightOn)
         {
-            flashlightOn = false;
-            StartCoroutine(lightDelayed());
-        }
-    }
-
-    private IEnumerator lightDelayed()
-    {
-        if(flashlightOn)
-        {
-            yield return new WaitForSeconds(0.2f);
-            flashlightTrigger.Play();
-            lightSource.SetActive(true);
-        }
-        else
-        {
-            flashlightTrigger.Play();
             lightSource.SetActive(false);
-            yield return new WaitForSeconds(0.15f);
-            animator.SetBool("FlashLightOn", false);
+            flashlightOn = false;
         }
     }
 }
