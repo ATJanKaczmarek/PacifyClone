0fb7191fbe6a649dbb86e5f58538ccc03cad2bbb
ATJanKaczmarek <53752609+ATJanKaczmarek@users.noreply.github.com>
2020-06-12 03:03:22 +0200

    Added Croshair, Added Running

--- PacifyClone/Assets/Scripts/PlayerMovement.cs
+++ PacifyClone/Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 12.0f;
+    public float speed = 6.0f;
     public float gravity = -9.81f;
     public float groundDistance = 0.4f;
     public float jumpHeight = 3f;
     public Transform groundCheck;
     public LayerMask groundMask;
+    public float runSpeed = 12.0f;
+
+    public AudioClip[] footsteps;
+    public AudioSource source;
+    private bool soundFinished = true;
 
     private Vector3 velocity;
     private bool isGrounded;
+    private bool isRunning;
     private void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -23,12 +29,35 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
+        if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        if (!isRunning)
+            controller.Move(move * speed * Time.deltaTime);
+        else if (isRunning)
+            controller.Move(move * runSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (isGrounded && soundFinished)
+            {
+                int randomNumber = Random.Range(0, footsteps.Length);
+                source.clip = footsteps[randomNumber];
+                source.Play();
+                StartCoroutine(WaitForEndOfSound());
+            }
+        }
 
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
@@ -37,5 +66,13 @@ public class PlayerMovement : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    private IEnumerator WaitForEndOfSound()
+    {
+        soundFinished = false;
+        yield return new WaitWhile(() => source.isPlaying);
+        yield return new WaitForSeconds(0.125f);
+        soundFinished = true;
     }
 }
